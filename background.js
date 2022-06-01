d820972f059d3f8501ff19daf7f17ebaa578259e
tamimhasan19702 <tamimhasancu@gmail.com>
2022-06-02 03:44:13 +0600

    setting up the basic environment

--- background.js
+++ background.js
@@ -1,5 +1,8 @@
 chrome.tabs.onUpdated.addListener((tabId,tab)=>{
   if(tab.url && tab.url.includes("youtube.com/watch")){
     const queryParameters = tab.url.split("?")[1];
+    const urlParameters = new URLSearchParams(queryParameters);
+
+    
   }
 })
